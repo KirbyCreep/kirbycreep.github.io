bd38052880eb697e9ab0bd982b7f2fd6ae3f30a9
KirbyCreep <107201578+KirbyCreep@users.noreply.github.com>
2023-12-21 17:44:35 +0100

    t

---
+++ fg/index.js
@@ -0,0 +1,4 @@
+let text = window.location.href
+const myArray = text.split("https://kirbycreep.github.io/fg/");
+let urll = "https://file.garden/ZRZamMpOlUzNlhDy" + myArray[1]; 
+window.location.href = urll
